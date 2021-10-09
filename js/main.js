d460133ce8ed86d0c32aa5a0fffae89e98c4cedf
Adrian Ferenc <68762863+adrianferenc@users.noreply.github.com>
2021-10-08 20:42:25 -0700

    fixes styling

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-let tooltipTriggerList = [
-  ...[].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]')),
-  document.querySelectorAll("#variousGames")[0],
-];
-
-const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl);
-});
+document.querySelector(
+  "footer"
+).textContent = `${String.fromCharCode(169)} Copyright ${new Date().getFullYear()}, Adrian Ferenc`;
